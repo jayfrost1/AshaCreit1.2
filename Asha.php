e2a4aae026ab880f46b24f4c4d050cf575f04219
Japhet Gondwe <japhetgondwe20@gmail.com>
2021-11-12 20:40:34 +0200

    first commit

---
+++ Asha.php
@@ -0,0 +1,52 @@
+<?php
+
+
+    if ( $text == "" )
+        {
+            $response  = "Welcome to Asha\n";
+            $response .= "1. Enter 1 to continue";
+        }
+
+// when 1 is selectd following screen shows
+    else if ($text == "1")
+        {
+            $response  = "AshaCredit, VillageBanking you can rely on\n";
+            $response .= "1. AshaStarter - Register \n";
+            $response .= "2. Check Balance \n";
+            $response .= "3. Deposit into account \n";
+            $response .= "4. Invest \n";
+        }
+
+
+
+    else if ($text == "1*1")
+        {
+            $response = "AshaStarter - Registration \n";
+            $a = readline('Enter a string: ');
+            echo $a;
+        }
+
+  else if ($text == "1*2")
+        {
+            $response = "Check Balance \n";
+            $a = readline('Enter a string: ');
+            echo $a;
+        }
+
+
+          else if ($text == "1*3")
+        {
+            $response = "Deposit into account \n";
+            $a = readline('Enter a string: ');
+            echo $a;
+        }
+
+
+          else if ($text == "1*4")
+        {
+            $response = "Invest \n";
+            $a = readline('Enter a string: ');
+            echo $a;
+        }
+
+?>
